867acf7043800c2fee4302f5fca10143d669aaf1
xNemix <46863616+xNemix@users.noreply.github.com>
2022-12-12 10:02:09 +0100

    Lagt til Teller oppgaven

---
+++ TellerOppgave/Moodle/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Moodle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var range = 250;
+            var counts = new int[range];
+            int totalLetters = 0;
+            string text = "something";
+
+            while (!string.IsNullOrWhiteSpace(text))
+            {
+                text = Console.ReadLine() ?? string.Empty;
+                foreach (var character in text.ToUpper())
+                {
+                    totalLetters++;
+                    counts[(int)character]++;
+                }
+                for (var i = 0; i < range; i++)
+                {
+                    if (counts[i] > 0)
+                    {
+                        var character = (char)i;
+                        var percentage = 100 * (double)counts[i] / totalLetters;
+                        string output = character + " - " + percentage.ToString("F2") + "%";
+                        Console.CursorLeft = Console.BufferWidth - output.Length - 1;
+                        Console.WriteLine(output);
+                    }
+                }
+            }
+        }
+    }
+}
